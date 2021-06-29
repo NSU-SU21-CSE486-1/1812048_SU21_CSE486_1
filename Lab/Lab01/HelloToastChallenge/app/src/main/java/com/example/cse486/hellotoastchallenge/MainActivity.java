2e0bd92471bb8476b1c323fc55356b2d4f5b5deb
Afra <afra.nawar05@northsouth.edu>
2021-06-30 01:26:11 +0600

    Lab01 Coding Challenge Bug fixes

--- Lab/Lab01/HelloToastChallenge/app/src/main/java/com/example/cse486/hellotoastchallenge/MainActivity.java
+++ Lab/Lab01/HelloToastChallenge/app/src/main/java/com/example/cse486/hellotoastchallenge/MainActivity.java
@@ -14,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mShowCount = (TextView) findViewById(R.id.show_count);
+        
+        
     }
 
     public void countUp(View view) {
