bbe0cb2c3f92430668012681a85fe18cffe22335
Afra <afra.nawar05@northsouth.edu>
2021-07-20 20:21:18 +0600

    #7 Added Phone and Email Screen Assgn1

---
+++ Theory/Assignment/Assignment01/app/src/main/java/com/example/cse486/project01/FinalActivity.java
@@ -0,0 +1,14 @@
+package com.example.cse486.project01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final);
+    }
+}
