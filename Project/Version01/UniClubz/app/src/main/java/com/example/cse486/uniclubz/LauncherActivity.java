1b036ea1daf5abefa07337b064cb1f07dd5cf17c
Afra <afra.nawar05@northsouth.edu>
2021-07-22 20:48:34 +0600

    initiated launcher activity

--- Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/LauncherActivity.java
+++ Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/LauncherActivity.java
@@ -2,24 +2,26 @@ package com.example.cse486.uniclubz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LauncherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_launcher);
 
         UserPref userPref = new UserPref(getApplicationContext());
 
-        if(!userPref.isLoggedIn())
+        if(userPref.isLoggedIn())
         {
-
+            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
         }
         else
         {
 
         }
+
     }
 }
