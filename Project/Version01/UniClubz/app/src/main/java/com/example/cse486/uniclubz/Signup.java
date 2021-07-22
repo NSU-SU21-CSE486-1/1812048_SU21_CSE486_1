1b272d68f4b4ea729ebaf126b07e417d9dba3f6f
Afra <afra.nawar05@northsouth.edu>
2021-07-23 03:20:42 +0600

    #12 signup layout completed

--- Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/Signup.java
+++ Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/Signup.java
@@ -1,8 +1,11 @@
 package com.example.cse486.uniclubz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+
+import com.example.cse486.uniclubz.databinding.ActivitySignupBinding;
 
 public class Signup extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        ActivitySignupBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_signup);
+
     }
 }
