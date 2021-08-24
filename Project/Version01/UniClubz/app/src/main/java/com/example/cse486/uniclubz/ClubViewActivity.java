bf7549e22d31d4da62bb6fea555af289491a9b57
Afra <afra.nawar05@northsouth.edu>
2021-08-24 12:08:34 +0600

    #18 Club View Activity initialized UniCLubz

---
+++ Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/ClubViewActivity.java
@@ -0,0 +1,13 @@
+package com.example.cse486.uniclubz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClubViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_club_view);
+    }
+}
