eb63c77ed49dab5997d2a2e62927771feee68799
Afra <afra.nawar05@northsouth.edu>
2021-08-26 09:01:47 +0600

    #18 Defined Club class UniClubz

--- Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/modelclasses/Club.java
+++ Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/modelclasses/Club.java
@@ -1,4 +1,20 @@
 package com.example.cse486.uniclubz.modelclasses;
 
 public class Club {
+    private String cname;
+    private String uni;
+    private String[] admins;
+    private String[] members;
+
+    public Club(String cname, String uni) {
+        this.cname = cname;
+        this.uni = uni;
+    }
+
+    public Club(String cname, String uni, String[] admins, String[] members) {
+        this.cname = cname;
+        this.uni = uni;
+        this.admins = admins;
+        this.members = members;
+    }
 }
