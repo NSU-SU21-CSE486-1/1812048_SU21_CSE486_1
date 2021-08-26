da523939ec7670469623377024082a9dced29748
Afra <afra.nawar05@northsouth.edu>
2021-08-26 09:05:16 +0600

    #17 Defined University class UniClubz

--- Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/modelclasses/University.java
+++ Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/modelclasses/University.java
@@ -1,4 +1,29 @@
 package com.example.cse486.uniclubz.modelclasses;
 
 public class University {
+    private String uniname;
+    private String sid;
+    private String dept;
+    private String level;
+    private String uniemail;
+
+
+    public University(String uniname, String sid, String dept, String level, String uniemail) {
+        this.uniname = uniname;
+        this.sid = sid;
+        this.dept = dept;
+        this.level = level;
+        this.uniemail = uniemail;
+    }
+
+    @Override
+    public String toString() {
+        return "University{" +
+                "uniname='" + uniname + '\'' +
+                ", sid='" + sid + '\'' +
+                ", dept='" + dept + '\'' +
+                ", level='" + level + '\'' +
+                ", uniemail='" + uniemail + '\'' +
+                '}';
+    }
 }
