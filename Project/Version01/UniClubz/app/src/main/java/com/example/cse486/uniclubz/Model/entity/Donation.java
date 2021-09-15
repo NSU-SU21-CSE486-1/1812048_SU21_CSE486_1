8e147701b5e01e30106d173b084960efbd54fb2c
Afra <afra.nawar05@northsouth.edu>
2021-09-16 02:11:07 +0600

    #20 Donation Entity UniClubz

--- Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/Model/entity/Donation.java
+++ Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/Model/entity/Donation.java
@@ -1,4 +1,24 @@
 package com.example.cse486.uniclubz.Model.entity;
 
 public class Donation {
+    String patient;
+    String hospital;
+    String address;
+    String contactname;
+    String contact;
+    String date;
+    String bgroup;
+
+    public Donation(String patient, String hospital, String address, String contactname, String contact, String date, String bgroup) {
+        this.patient = patient;
+        this.hospital = hospital;
+        this.address = address;
+        this.contactname = contactname;
+        this.contact = contact;
+        this.date = date;
+        this.bgroup = bgroup;
+    }
+
+
+
 }
