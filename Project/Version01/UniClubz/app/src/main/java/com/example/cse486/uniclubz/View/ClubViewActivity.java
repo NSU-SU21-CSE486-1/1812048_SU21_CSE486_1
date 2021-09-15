244e567ba172f7254cc918c0641cfdbf5a091303
Afra <afra.nawar05@northsouth.edu>
2021-09-16 02:11:34 +0600

    #18 Club View Activity layout Added UniClubz

--- Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/View/ClubViewActivity.java
+++ Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/View/ClubViewActivity.java
@@ -2,6 +2,7 @@ package com.example.cse486.uniclubz.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.cse486.uniclubz.R;
 
@@ -11,5 +12,23 @@ public class ClubViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club_view);
+    }
+
+    public void toevents(View view) {
+    }
+
+    public void tobd(View view) {
+    }
+
+    public void addevents(View view) {
+    }
+
+    public void adddonation(View view) {
+    }
+
+    public void addmem(View view) {
+    }
+
+    public void addadmin(View view) {
     }
 }
