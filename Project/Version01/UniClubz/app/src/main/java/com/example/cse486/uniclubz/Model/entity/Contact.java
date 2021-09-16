33e11e90f03afeacc5578749029322207d8718a5
Afra <afra.nawar05@northsouth.edu>
2021-09-16 23:22:56 +0600

    #17 Contact Fragment Modified UniClubz

--- Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/Model/entity/Contact.java
+++ Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/Model/entity/Contact.java
@@ -1,7 +1,40 @@
 package com.example.cse486.uniclubz.Model.entity;
 
-public class Contact {
+import java.io.Serializable;
 
+public class Contact implements Serializable {
 
+    String type;
+    String contact;
+    String tag;
 
+    public Contact(String type, String contact, String tag) {
+        this.type = type;
+        this.contact = contact;
+        this.tag = tag;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
 }
--- Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/View/ContactInfoFragment.java
+++ Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/View/ContactInfoFragment.java
@@ -1,66 +1,144 @@
 package com.example.cse486.uniclubz.View;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import com.example.cse486.uniclubz.Model.entity.Contact;
 import com.example.cse486.uniclubz.R;
+import com.example.cse486.uniclubz.ViewModel.SerializableManager;
+import com.example.cse486.uniclubz.ViewModel.UserPref;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ContactInfoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 public class ContactInfoFragment extends Fragment {
+    ArrayList<Contact> woi=new ArrayList<>();
+    View view;
+    ListView listView;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public ContactInfoFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ContactInfoFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static ContactInfoFragment newInstance(String param1, String param2) {
         ContactInfoFragment fragment = new ContactInfoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_info, container, false);
+        UserPref userInfo = new UserPref(getContext());
+      //  FragmentPhonesBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_phones, container, false);
+        view = inflater.inflate(R.layout.fragment_contact_info, container, false);
+        listView = view.findViewById(R.id.display_list2);
+
+
+
+        woi = SerializableManager.readSerializable(getContext(), "phones.txt");
+
+        if(woi == null || woi.size()<=1)
+        {
+            woi = new ArrayList<Contact>();
+            Contact phone1 = new Contact("Phone","123456789","Home");
+            Contact phone2 = new Contact("Phone","987654211","Office");
+            Contact phone3 = new Contact("Phone","333444555","Work");
+
+            woi.add(phone1);
+            woi.add(phone2);
+            woi.add(phone3);
+
+
+        //    SerializableManager.saveSerializable(getContext(),woi,"phones.txt");
+
+        }
+
+        setuplistview(woi);
+
+
+        view.findViewById(R.id.addphonebtn).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getContext(),AddContactActivity.class);
+                    startActivityForResult(intent,1);
+
+                }
+            });
+
+            return view;
+
+        }
+
+        public void setuplistview(ArrayList<Contact> woi)
+        {
+            ArrayList<String> names = new ArrayList<String>();
+            ArrayList<String> nid = new ArrayList<String>();
+            for (Contact a:
+                    woi) {
+                names.add(a.getTag());
+                nid.add(a.getContact());
+
+            }
+            String[] tagsarr = new String[woi.size()];
+            names.toArray(tagsarr);
+            String[] numarr = new String[woi.size()];
+            nid.toArray(numarr);
+            List<Map<String, String>> data = new ArrayList<Map<String, String>>();
+            for (int i=0;i<woi.size();i++) {
+                Map<String, String> datum = new HashMap<String, String>(2);
+                datum.put("desc", tagsarr[i]);
+                datum.put("txt", numarr[i]);
+                data.add(datum);
+            }
+            SimpleAdapter adapter = new SimpleAdapter(getActivity().getBaseContext(), data,
+                    android.R.layout.simple_list_item_2,
+                    new String[] {"desc", "txt"},
+                    new int[] {android.R.id.text1,
+                            android.R.id.text2});
+
+            listView.setAdapter(adapter);
+         //  SerializableManager.removeSerializable(getActivity().getBaseContext(),"phones.txt");
+            SerializableManager.saveSerializable(getActivity().getBaseContext(),woi,"phones.txt");
+        }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1)
+        {
+            if(data!=null && resultCode!= RESULT_OK) {
+                Contact phone = new Contact(data.getStringExtra("type"), data.getStringExtra("phone"),data.getStringExtra("tag"));
+                woi.add(phone);
+                setuplistview(woi);
+            }
+
+          // Toast.makeText(getActivity().getBaseContext(), phone.getNumber(), Toast.LENGTH_SHORT).show();
+        }
     }
+
+
+
 }
