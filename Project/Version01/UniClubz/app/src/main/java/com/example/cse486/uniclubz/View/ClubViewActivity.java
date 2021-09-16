9c2095c3e3b605e16c81bf7bb8eac4bae91a23f0
Afra <afra.nawar05@northsouth.edu>
2021-09-16 12:59:35 +0600

    #19 Add Event Layout UniClubz

--- Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/View/ClubViewActivity.java
+++ Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/View/ClubViewActivity.java
@@ -1,6 +1,8 @@
 package com.example.cse486.uniclubz.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -15,20 +17,32 @@ public class ClubViewActivity extends AppCompatActivity {
     }
 
     public void toevents(View view) {
+        Intent intent = new Intent(getApplicationContext(),EventListActivity.class);
+        startActivity(intent);
     }
 
     public void tobd(View view) {
+        Intent intent = new Intent(getApplicationContext(),BloodDonationListActivity.class);
+        startActivity(intent);
     }
 
     public void addevents(View view) {
+        Intent intent = new Intent(getApplicationContext(),AddDonationActivity.class);
+        startActivity(intent);
     }
 
     public void adddonation(View view) {
+        Intent intent = new Intent(getApplicationContext(),AddDonationActivity.class);
+        startActivity(intent);
     }
 
     public void addmem(View view) {
+        Intent intent = new Intent(getApplicationContext(),AddDonationActivity.class);
+        startActivity(intent);
     }
 
     public void addadmin(View view) {
+        Intent intent = new Intent(getApplicationContext(),AddDonationActivity.class);
+        startActivity(intent);
     }
 }
