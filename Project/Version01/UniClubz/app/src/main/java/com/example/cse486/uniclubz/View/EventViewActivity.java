c40f77629e599839c0a67c67fb1abf8253cf306f
Afra <afra.nawar05@northsouth.edu>
2021-09-16 23:00:56 +0600

    #19 Event Activity Modified UniClubz

--- Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/View/EventViewActivity.java
+++ Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/View/EventViewActivity.java
@@ -2,14 +2,32 @@ package com.example.cse486.uniclubz.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.example.cse486.uniclubz.Model.entity.Event;
 import com.example.cse486.uniclubz.R;
 
 public class EventViewActivity extends AppCompatActivity {
+
+    TextView ename,cname,uname,date,edesc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_view);
+
+        Event event = (Event) getIntent().getSerializableExtra("event");
+
+        ename = findViewById(R.id.evname);
+        cname = findViewById(R.id.cname);
+        date = findViewById(R.id.date);
+        edesc = findViewById(R.id.edesc);
+        uname = findViewById(R.id.uniname);
+
+        ename.setText(event.getEname());
+        cname.setText(event.getEclub());
+        uname.setText(event.getEuni());
+        edesc.setText(event.getEdesc());
+
     }
 }
