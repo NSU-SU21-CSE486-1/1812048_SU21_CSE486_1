b61bec9971976b14cfdc9d50dd71a941fb860e18
Afra <afra.nawar05@northsouth.edu>
2021-09-16 12:45:35 +0600

    #20 Blood Donation List Code UniClubz

--- Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/Model/Repository/DonationRepository.java
+++ Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/Model/Repository/DonationRepository.java
@@ -2,11 +2,30 @@ package com.example.cse486.uniclubz.Model.Repository;
 
 import com.example.cse486.uniclubz.Model.DAO.DonationDAO;
 import com.example.cse486.uniclubz.Model.entity.Club;
+import com.example.cse486.uniclubz.Model.entity.Donation;
 import com.example.cse486.uniclubz.Model.entity.University;
+
+import java.util.ArrayList;
 
 public class DonationRepository implements DonationDAO {
 
 
+    public ArrayList<Donation> getSampleDonations()
+    {
+        ArrayList<Donation> donations = new ArrayList<>();
+        Donation donation = new Donation("Mr.XYZ", "City Hospital", "Banani, Dhaka", "Mr.PQRS", "4324334","14/7/2022","O+");
+
+        Donation donation1 = donation;
+        Donation donation2 = donation;
+        Donation donation3 = donation;
+        Donation donation4 = donation;
+
+        donations.add(donation);
+        donations.add(donation1);
+        donations.add(donation2);
+        donations.add(donation3);
+        return donations;
+    }
     @Override
     public void createDonation(Club club) {
 
--- Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/View/BloodDonationListActivity.java
+++ Project/Version01/UniClubz/app/src/main/java/com/example/cse486/uniclubz/View/BloodDonationListActivity.java
@@ -1,16 +1,37 @@
 package com.example.cse486.uniclubz.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.cse486.uniclubz.Model.Repository.DonationRepository;
+import com.example.cse486.uniclubz.Model.entity.Club;
+import com.example.cse486.uniclubz.Model.entity.Donation;
 import com.example.cse486.uniclubz.R;
+import com.example.cse486.uniclubz.View.Adapter.ClubAdapter;
+import com.example.cse486.uniclubz.View.Adapter.DonationAdapter;
+
+import java.util.ArrayList;
 
 public class BloodDonationListActivity extends AppCompatActivity {
 
+    DonationRepository repository;
+    DonationAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blood_donation_list);
+
+
+        ArrayList<Donation> donations = repository.getSampleDonations();
+
+        adapter= new DonationAdapter(donations,getApplicationContext());
+        RecyclerView recyclerView = findViewById(R.id.crv);
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
     }
 }
